04ed2295ce1885a2e10271ed20644f9222226b22
Essem <smswessem@gmail.com>
2021-07-24 22:31:33 -0500

    Finished every package on the list, overhauled package building, added scripts

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+./build-archive.sh
+
+cd mips-n64-binutils
+makepkg -fsi
+
+cd ../mips-n64-gcc
+makepkg -fs
+
+cd ../root-compatibility-environment
+makepkg -f
+
+cd ../vadpcm-tools
+makepkg -fs
+
+cd ../spicy
+makepkg -fs
+
+cd ../makemask
+makepkg -fs
+
+cd ../n64graphics
+makepkg -fs
+
+cd ../libstdc++296
+CARCH="i686" linux32 makepkg -fs
